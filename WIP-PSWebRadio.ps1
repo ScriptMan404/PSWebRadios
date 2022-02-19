86c18fdcd4d0f6abd55b5db5220bba0b4d40b789
ScriptMan404 <45128030+ScriptMan404@users.noreply.github.com>
2022-02-19 18:37:35 +0100

    Add files via upload

---
+++ WIP-PSWebRadio.ps1
@@ -0,0 +1,25 @@
+﻿$NL = echo ""
+
+$NL
+$Search = Read-Host "Search for webradios"
+
+$Webradios = Invoke-RestMethod "https://fr1.api.radio-browser.info/json/stations/search?name=$Search"
+
+$NL
+$Menu = @{}
+    for ($i=1;$i -le $Webradios.Count; $i++) {
+        Write-Host "$i. $($Webradios[$i-1].name)"
+        $Menu.Add($i,($Webradios[$i-1].name))
+    }
+$NL
+[int]$Choice = Read-Host "Enter selection"
+$WRStream = $Webradios[$Choice-1]
+$NL
+Write-Host -ForegroundColor Green "Now playing :" $WRStream.name
+
+if ($VLCProcess -eq $null) {
+    $VLCProcess = Start-Process -WindowStyle Hidden -PassThru -FilePath "vlc.exe" -WorkingDirectory "C:\Program Files\VideoLAN\VLC" -ArgumentList $WRStream.url_resolved
+} else {
+    $VLCProcess | Stop-Process
+    $VLCProcess = Start-Process -WindowStyle Hidden -PassThru -FilePath "vlc.exe" -WorkingDirectory "C:\Program Files\VideoLAN\VLC" -ArgumentList $WRStream.url_resolved
+}
